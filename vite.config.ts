eb2142fd3c1f8e13f8bb78e8dbf198d5422ad0be
junheelee <macjjuni@gmail.com>
2023-02-03 11:50:45 +0900

    chore(vite.config): 주석 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  console.log(env)
-
   return {
     plugins: [
       react(),
