dcbc0d306388ba625ce7eaaa52006922f383739c
macjjuni <macjjuni@gmail.com>
2023-07-17 23:10:49 +0900

    Feat: 크립토 공포탐욕지수 테스트 페이지 작성

---
+++ src/pages/Test/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <img src="https://alternative.me/crypto/fear-and-greed-index.png" alt="crypto fear greed index chart" />
+    </div>
+  )
+}
+
+export default index
