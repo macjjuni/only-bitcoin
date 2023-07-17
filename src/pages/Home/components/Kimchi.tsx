0db390e568dffb096d1a87b5ce793d11334cf1ff
JunHee <macjjuni@strato.co.kr>
2023-07-17 13:10:18 +0900

    Style: 김치 이미지 변경

--- src/pages/Home/components/Kimchi.tsx
+++ src/pages/Home/components/Kimchi.tsx
@@ -1,12 +1,11 @@
 import { useLayoutEffect, useState, useCallback } from 'react'
 import { Box } from '@mui/material'
+import { BiSolidInfoCircle } from 'react-icons/bi'
 import CountText from '@/components/CountText'
 import { useBearStore } from '@/zustand/store'
 import { calcPerDiff } from '@/utils/common'
 import { getExRate } from '@/api/exRate'
 import ExRateDialog from '@/components/modal/ExRateDialog'
-
-// const alertText = (txt: number) => `USD/KRW: ${txt.toString()}`
 
 const Kimchi = () => {
   const { exRate, btc, setExRate } = useBearStore((state) => state)
@@ -33,7 +32,7 @@ const Kimchi = () => {
   return (
     <>
       <Box onClick={showDialog} position="absolute" top="-28px" right="0px" display="flex" flexDirection="row" justifyContent="flex-end" alignItems="center" height={30} gap="4px" fontSize={20}>
-        <img src="/images/kimchi.png" alt="김치 이미지" width={40} className="kimchi-img" />
+        <BiSolidInfoCircle fontSize={20} className="kimchi-color kimchi-info" />
         <CountText text={calcPerDiff(btc.krw, btc.usd, exRate.basePrice)} className="kimchi-color" duration={0.3} percent decimals={2} />
       </Box>
       <ExRateDialog open={isEx} setOpen={setEx} kimpPrice={calcPerDiff(btc.krw, btc.usd, exRate.basePrice)} />
