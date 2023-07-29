84f41eb186694de95cc587d356336d5646f056ab
macjjuni <macjjuni@gmail.com>
2023-07-29 16:07:55 +0900

    Style: Chip 컴포넌트 사이즈별 폰트크기 변경

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -8,15 +8,13 @@ const ChipItem = ({ label, value }: { label: string; value: string }) => {
   const generateLabel = useMemo(() => {
     return (
       <Stack flexDirection="row" gap="5px">
-        <span className="price-txt-xs">{label}</span>
-        <span className="price-txt-xs" style={{ textShadow: '1px 1px 1px #d0d0d0' }}>
-          {value}
-        </span>
+        <span>{label}</span>
+        <span style={{ textShadow: '1px 1px 1px #d0d0d0' }}>{value}</span>
       </Stack>
     )
   }, [value])
 
-  return <Chip className="chip-txt" label={generateLabel} variant="outlined" size={isSmall ? 'small' : 'medium'} sx={{ borderRadius: 0 }} />
+  return <Chip className="chip-txt" label={generateLabel} variant="outlined" size={isSmall ? 'small' : 'medium'} sx={{ borderRadius: 0, fontSize: isSmall ? '13px' : '18px' }} />
 }
 
 export default ChipItem
