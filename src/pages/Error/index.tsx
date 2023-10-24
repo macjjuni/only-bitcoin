b194a0b09611058d87d7705951ec054e021ec545
macjjuni <macjjuni@gmail.com>
2023-10-24 15:51:09 +0900

    Fix: Props 누락 오류 수정

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -14,7 +14,7 @@ const Error = () => {
   return (
     <Stack className="error-page">
       <Typography component="h1" align="center" fontSize={34} fontWeight="bold">
-        <LottieItem option={lottieOption} animationData={Bitcoin404} speed={1} />
+        <LottieItem play option={lottieOption} animationData={Bitcoin404} speed={1} />
       </Typography>
       <Stack flexDirection="row" justifyContent="center" py={4}>
         <Button
