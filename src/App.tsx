f7d0030801a1946b991cd45d652c79da19ac71e6
macjjuni <macjjuni@gmail.com>
2023-11-02 13:14:53 +0900

    Refactor: 로딩 로티 제거, 소켓 시작지점 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,31 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Header from '@/layout/header'
 import Main from '@/layout/main'
 import Footer from '@/layout/footer'
-import GoogleGA from './components/init/GoogleGA'
-import AdsenseCodeSnippet from './components/init/AdSenseCodeSnippet'
-import BlockHeight from './components/init/BlockHeight'
+import Initializer from './components/initial/Initializer'
 
 import { darkTheme, lightTheme } from './styles/theme'
 import { useBearStore } from './zustand/store'
 import { toastProps } from '@/data/toast'
 
 const toastOptions = toastProps()
-const isDev = import.meta.env.MODE === 'development'
 
 const App = () => {
+  // 소켓 초기화
   const theme = useBearStore((state) => state.theme)
-  GoogleGA()
   return (
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      {!isDev && <AdsenseCodeSnippet />}
-      <BlockHeight />
-      <div id="only-bitcoin" className={theme}>
-        <CssBaseline />
-        <Header />
-        <Main>
-          <Outlet />
-        </Main>
-        <Footer />
-        <ToastContainer {...toastOptions} />
-      </div>
-    </ThemeProvider>
+    <Initializer>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <div id="only-bitcoin" className={theme}>
+          <CssBaseline />
+          <Header />
+          <Main>
+            <Outlet />
+          </Main>
+          <Footer />
+          <ToastContainer {...toastOptions} />
+        </div>
+      </ThemeProvider>
+    </Initializer>
   )
 }
 
--- src/layout/main/index.tsx
+++ src/layout/main/index.tsx
@@ -1,29 +1,9 @@
-import { useEffect, useLayoutEffect, useState } from 'react'
 import { Container } from '@mui/material'
 import { layout } from '@/styles/style'
-import Spinner from '@/components/loading/Spinner'
-
-import initUpbit from '@/socket/upbit'
-import initBinance from '@/socket/binance'
-
-import { useBearStore } from '@/zustand/store'
 
 const Main = ({ children }: { children: JSX.Element }) => {
-  const [load, setLoad] = useState(false) // 렌더링(소켓 연결)
-  const { btc } = useBearStore((state) => state)
-
-  useEffect(() => {
-    if (!load && btc.krw !== 0) setLoad(true) // 시세 변동 시 계산 => 코인 개수를 기준으로 가격 변환
-  }, [btc])
-
-  useLayoutEffect(() => {
-    initBinance()
-    initUpbit()
-  }, [])
-
   return (
     <Container component="main" className="main" sx={{ minHeight: `calc(100dvh - ${layout.main}px)` }}>
-      <Spinner isRender={load} />
       {children}
     </Container>
   )
