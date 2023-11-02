7f91c2fc3083707ffe211722010c5a296a538e33
macjjuni <macjjuni@gmail.com>
2023-11-02 11:19:03 +0900

    Refactor: 블록 상태 조회 컴포넌트 interval함수 리팩토링

---
+++ src/components/init/BtcBlockInit.tsx
@@ -0,0 +1,36 @@
+import { memo, useCallback, useLayoutEffect } from 'react'
+import { useBearStore, bearStore } from '@/zustand/store'
+import { getBtcRecentBlockHeight } from '@/api/mempool'
+import interval from '@/utils/interval'
+
+const intervalTime = 120000 // Interval Time(ms): 2분
+
+const BtcBlockInit = () => {
+  const blockData = useBearStore((state) => state.blockData)
+
+  // 비트코인 블록 데이터 초기화
+  const updateBlockHeight = useCallback(async () => {
+    console.log('Get Block Status!')
+    const { height, timeStamp } = await getBtcRecentBlockHeight()
+    bearStore.updateBlock({ height, timeStamp, updateTimeStamp: Number(new Date()) })
+  }, [])
+  // 업데이트 시간 체크해서 업데이트 실행
+  const updateCheck = useCallback(() => {
+    if (blockData.height === 0) updateBlockHeight()
+    else {
+      const now = Date.now()
+      const timeDiff = now - blockData.updateTimeStamp
+      if (timeDiff >= intervalTime) updateBlockHeight() // 업데이트한지 2분 이후면 업데이트 재실행
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    updateCheck()
+    const blockInterval = interval(updateBlockHeight, intervalTime)
+    blockInterval.start()
+  }, [])
+
+  return null
+}
+
+export default memo(BtcBlockInit)
