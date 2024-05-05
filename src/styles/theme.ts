0a58f6612cd8879b416a9f376cddb0b5fed44bc4
macjjuni <macjjuni@strato.co.kr>
2024-05-06 03:44:22 +0900

    feature. 폰트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,21 @@
 import { createTheme } from '@mui/material/styles'
 
+const defaultFont = {
+  typography: {
+    fontFamily: 'Pretendard, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+  },
+}
+
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
+  ...defaultFont,
 })
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
   },
+  ...defaultFont,
 })
