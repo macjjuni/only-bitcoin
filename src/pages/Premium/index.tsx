bbbfa4bfa743a09e1d27f63d4f0dd748559d6f49
macjjuni <macjjuni@strato.co.kr>
2024-05-06 23:50:54 +0900

    feature. 프리미엄 페이지 세부 스타일 수정

--- src/pages/Premium/index.tsx
+++ src/pages/Premium/index.tsx
@@ -2,6 +2,7 @@ import { useMemo, ReactNode, memo } from 'react'
 import { Stack, Typography, useMediaQuery } from '@mui/material'
 import { FaWonSign } from 'react-icons/fa'
 import { IoLogoUsd } from 'react-icons/io'
+import { TbSquareRoundedLetterK } from 'react-icons/tb'
 import { MdOutlinePercent } from 'react-icons/md'
 import CardItem from '@/components/molecule/CardItem'
 import { useBearStore } from '@/store'
@@ -17,8 +18,6 @@ const MemoizedLabel = memo(LabelElement)
 const PremiumPage = () => {
   const { krw, usd } = useBearStore((state) => state.btc)
   const { basePrice } = useBearStore((state) => state.exRate)
-  //   const kimchi = useBearStore((state) => state.)
-  //   console.log('kimchi', kimchi)
 
   const matches = useMediaQuery(`(min-width: ${responsive.mobile}px)`)
 
@@ -45,20 +44,33 @@ const PremiumPage = () => {
   const primiumKrw = useMemo(() => comma((krw - usaKrwPrice).toFixed(0)), [krw, usaKrwPrice]) // 원화 프리미엄 가격
   const primiumUsd = useMemo(() => comma((koreaUsdPrice - usd).toFixed(0)), [usd, koreaUsdPrice]) // 원화 프리미엄 가격
 
+  const stackJustifyContent = useMemo(() => (matches ? 'normal' : 'flex-end'), [matches])
+
   return (
     <Stack flexDirection="column" width="100%" height="100%" justifyContent="center">
       <PageTitle title="한국 프리미엄" />
       <Stack direction={matches ? 'row' : 'column'} spacing={1.5} sx={{ overflowX: 'auto' }} px={0.5} py={1}>
         <CardItem
-          title="프리미엄"
+          title={
+            <Stack flexDirection="row" alignItems="center" gap={1}>
+              <TbSquareRoundedLetterK fontSize={24} />
+              <Typography fontSize={22} fontWeight="bold">
+                프리미엄
+              </Typography>
+            </Stack>
+          }
           matches={matches}
           content={
             <Stack>
-              <Stack direction="row" alignItems="center" gap={1} fontSize={24} fontWeight="bold">
-                {WonIcon} {primiumKrw}
+              <Stack direction="row" justifyContent={stackJustifyContent} alignItems="center" gap={1} fontSize={24} fontWeight="bold">
+                {WonIcon}
+                {primiumKrw.includes('-') ? '' : '+'}
+                {primiumKrw}
               </Stack>
-              <Stack direction="row" alignItems="center" gap={1} fontSize={24} fontWeight="bold">
-                {DollarIcon} {primiumUsd}
+              <Stack direction="row" justifyContent={stackJustifyContent} alignItems="center" gap={1} fontSize={24} fontWeight="bold">
+                {DollarIcon}
+                {primiumUsd.includes('-') ? '' : '+'}
+                {primiumUsd}
               </Stack>
             </Stack>
           }
@@ -75,49 +87,53 @@ const PremiumPage = () => {
         />
         <CardItem
           title={
-            <Stack flexDirection="row" alignItems="center" gap={0.75}>
+            <Stack flexDirection="row" alignItems="center" gap={1}>
               <img src="/images/upbit_logo.webp" alt="upbit_logo" width={24} height={24} />
-              한국 시세
+              <Typography fontSize={22} fontWeight="bold">
+                한국 시세
+              </Typography>
             </Stack>
           }
           matches={matches}
           content={
             <Stack>
-              <Stack direction="row" alignItems="center" gap={1} fontSize={24} fontWeight="bold">
+              <Stack direction="row" justifyContent={stackJustifyContent} alignItems="center" gap={1} fontSize={24} fontWeight="bold">
                 {WonIcon} {comma(krw.toString())}
               </Stack>
-              <Stack direction="row" alignItems="center" gap={1} fontSize={24} fontWeight="bold">
+              <Stack direction="row" justifyContent={stackJustifyContent} alignItems="center" gap={1} fontSize={24} fontWeight="bold">
                 {DollarIcon} {comma(koreaUsdPrice.toFixed(0))}
               </Stack>
             </Stack>
           }
           bottom={
-            <Typography fontSize={18} fontWeight="bold">
-              환율: ₩{comma(basePrice.toString())}
+            <Typography fontSize={16} fontWeight="bold">
+              USD/KRW: {comma(basePrice.toString())}
             </Typography>
           }
         />
         <CardItem
           title={
-            <Stack flexDirection="row" alignItems="center" gap={0.75}>
+            <Stack flexDirection="row" alignItems="center" gap={1}>
               <img src="/images/binance_logo.webp" alt="binance_logo" width={24} height={24} />
-              해외 시세
+              <Typography fontSize={22} fontWeight="bold">
+                해외 시세
+              </Typography>
             </Stack>
           }
           matches={matches}
           content={
             <Stack>
-              <Stack direction="row" alignItems="center" gap={1} fontSize={24} fontWeight="bold">
+              <Stack direction="row" justifyContent={stackJustifyContent} alignItems="center" gap={1} fontSize={24} fontWeight="bold">
                 {WonIcon} {comma(usaKrwPrice.toFixed(0))}
               </Stack>
-              <Stack direction="row" alignItems="center" gap={1} fontSize={24} fontWeight="bold">
-                {DollarIcon} {comma(usd.toString())}
+              <Stack direction="row" justifyContent={stackJustifyContent} alignItems="center" gap={1} fontSize={24} fontWeight="bold">
+                {DollarIcon} {comma(usd.toFixed(0))}
               </Stack>
             </Stack>
           }
           bottom={
-            <Typography fontSize={18} fontWeight="bold">
-              환율: ₩{comma(basePrice.toString())}
+            <Typography fontSize={16} fontWeight="bold">
+              USD/KRW: {comma(basePrice.toString())}
             </Typography>
           }
         />
