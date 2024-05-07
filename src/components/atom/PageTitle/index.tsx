56bdffcdae18f847ceff5ee98176856f7d28646e
macjjuni <macjjuni@strato.co.kr>
2024-05-07 15:54:07 +0900

    tirivial. 스타일 수정

--- src/components/atom/PageTitle/index.tsx
+++ src/components/atom/PageTitle/index.tsx
@@ -12,7 +12,7 @@ const PageTitle = ({ title }: IPageTitle) => {
 
   return (
     <>
-      <Typography variant="h2" fontSize={24} fontWeight="bold" display="flex" justifyContent="flex-start" alignItems="center" gap="4px" mt={matches ? 4 : 2} mb={1.5}>
+      <Typography variant="h2" fontSize={24} fontWeight="bold" display="flex" justifyContent="flex-start" alignItems="center" gap={1} mt={matches ? 4 : 2} mb={1.5}>
         <BtcIcon size={24} />
         {title}
       </Typography>
