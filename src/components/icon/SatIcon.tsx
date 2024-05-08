b1183764dc60ca86510f2ded4338acd80750a0fd
macjjuni <macjjuni@gmail.com>
2024-05-08 17:51:16 +0900

    feature. 사토시 이미지 포멧 변환

--- src/components/icon/SatIcon.tsx
+++ src/components/icon/SatIcon.tsx
@@ -6,7 +6,7 @@ interface ISats {
 }
 
 const SatIcon = ({ width = 40, height = 40 }: ISats) => {
-  return <img src="./images/sat.jpeg" alt="Satoshi Symbol" className="sat-img" width={width} height={height} />
+  return <img src="./images/sat.webp" alt="Satoshi Symbol" className="sat-img" width={width} height={height} />
 }
 
 export default memo(SatIcon)
