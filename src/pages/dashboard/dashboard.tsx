656d663b43f72dc683ae6e145771065585b8295e
macjjuni <macjjuni@gmail.com>
2024-06-14 00:25:37 +0900

    feature. 레이아웃 변경

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,12 +1,12 @@
 import PageLayout from "@/layout/pageLayout/pageLayout";
-import MarketData from "@/pages/dashboard/components/marketData/marketData";
+// import MarketData from "@/pages/dashboard/components/marketData/marketData";
 import MarketChart from "@/pages/dashboard/components/marketChart/marketChart";
 import "./dashboard.scss";
 
 export default function Dashboard() {
   return (
     <PageLayout className="dashboard-page">
-      <MarketData />
+      {/* <MarketData /> */}
       <MarketChart />
     </PageLayout>
   );
