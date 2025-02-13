88bd73b4486548b71692d015302492a9079df4af
macjjuni <macjjuni@gmail.com>
2025-02-13 17:33:34 +0900

    trivial. 로딩 폴백 제거

--- src/widgets/hoc/withSuspence.tsx
+++ src/widgets/hoc/withSuspence.tsx
@@ -2,7 +2,7 @@ import React, { Suspense, ComponentType, ReactNode } from "react";
 
 export default function withSuspensePromise<P extends object>(
   WrappedComponent: ComponentType<P>,
-  fallback: ReactNode = <div>Loading...</div>
+  fallback: ReactNode = <></>
 ): (props: P) => React.JSX.Element {
   // eslint-disable-next-line react/display-name
   return (props: P) => (
