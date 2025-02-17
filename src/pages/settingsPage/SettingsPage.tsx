c216df89e67e9882b8d5201765d5b1943f0f1b95
macjjuni <macjjuni@gmail.com>
2025-02-17 18:06:50 +0900

    feature. Settings 페이지 개발 #2

--- src/pages/settingsPage/SettingsPage.tsx
+++ src/pages/settingsPage/SettingsPage.tsx
@@ -1,5 +1,5 @@
 import "./SettingsPage.scss";
-import { KSelect } from "kku-ui";
+import { KButton, KSelect, KSwitch } from "kku-ui";
 import { FormRow } from "@/widgets/settings/formRow/FormRow";
 import { TetherIcon, StarIcon, CurrencyIcon } from "@/shared/icons";
 import { btcColor } from "@/shared/constants/color";
@@ -10,15 +10,16 @@ export default function SettingsPage() {
     <div className="settings-page__area">
 
       <div className="settings-page__area__form__area">
-        <FormRow icon={<StarIcon color={btcColor} />}>
-           시작 페이지
+        <FormRow icon={<StarIcon color={btcColor} />} label="시작 페이지">
           <KSelect size="small" value="" width="160px" onChange={() => {}} items={[{title: 'Test', value: '123'}]} />
         </FormRow>
-        <FormRow icon={<TetherIcon size={24} />}>
-          USDT 기준 환율 적용
+        <FormRow icon={<TetherIcon size={24} />} label="USDT 기준 환율 적용">
+          <KSwitch value onChange={() => {}}  />
         </FormRow>
-        <FormRow icon={<CurrencyIcon size={24} color="#c2c2c2" />}>
-          가격 단위
+        <FormRow icon={<CurrencyIcon size={24} color="#c2c2c2" />} label="통화 단위">
+          <KButton label="KRW" size="small" />
+          <KButton label="USD" size="small" />
+          <KButton label="KRW/USD" size="small" />
         </FormRow>
       </div>
     </div>
--- src/widgets/settings/formRow/FormRow.tsx
+++ src/widgets/settings/formRow/FormRow.tsx
@@ -4,13 +4,15 @@ import "./FormRow.scss";
 interface FormRowTypes {
   children: ReactNode;
   icon?: ReactNode;
+  label?: ReactNode;
 }
 
-export const FormRow = ({children, icon}: FormRowTypes) => {
+export const FormRow = ({children, icon, label}: FormRowTypes) => {
   return (
     <div className="form-row__area">
       {icon && icon}
       <div className="form-row__area__content">
+        <div className="form-row__area__content__label">{label}</div>
         {children}
       </div>
     </div>
