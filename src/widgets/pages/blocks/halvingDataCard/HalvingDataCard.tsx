91fe6364602e8f0e538717dc9d2c419130bb3bb3
macjjuni <macjjuni@gmail.com>
2025-02-22 13:20:26 +0900

    refactor. HalvingDataCard 리팩토링

--- src/widgets/pages/blocks/halvingDataCard/HalvingDataCard.tsx
+++ src/widgets/pages/blocks/halvingDataCard/HalvingDataCard.tsx
@@ -1,43 +1,38 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
 import { KAccordion } from "kku-ui";
 import { blockHalvingData } from "@/shared/constants/block";
-import { comma } from "@/shared/utils/string";
 import "./HalvingDataCard.scss";
 
 
 const HalvingDataCard = () => {
 
-  // region [Hooks]
+  // region [Templates]
+
+  const HalvingDataList = useMemo(()=> [
+    {label: '순서', items: blockHalvingData.map(({ date }, idx) => ({value: idx+1, key: date}))},
+    {label: '날짜', items: blockHalvingData.map(({ date }) => ({value: date, key: date}))},
+    {label: '블록 높이', items: blockHalvingData.map(({ date, blockHeight }) => ({value: blockHeight, key: date}))},
+    {label: '채굴 보상(단위: btc)', items: blockHalvingData.map(({ blockReward, date }) => ({value: blockReward, key: date}))},
+  ], [])
+
   // endregion
+
 
   return (
     <KAccordion summary="반감기 정보" className="halving-data-card__area">
       <ul className="halving-data-card__area__list">
+        {
+          HalvingDataList.map(({label, items}) => (
 
-        <li className="halving-data-card__area__list__item">
-          <div className="halving-no">순서</div>
-          {blockHalvingData.map(({ date }, idx) => (
-            <div key={date} className="halving-no">{idx + 1}</div>
-          ))}
-        </li>
-        <li className="halving-data-card__area__list__item">
-          <div className="halving-date">날짜</div>
-          {blockHalvingData.map(({ date }) => (
-            <div key={date} className="halving-date">{date}</div>
-          ))}
-        </li>
-        <li className="halving-data-card__area__list__item">
-          <div className="halving-height">블록 높이</div>
-          {blockHalvingData.map(({ blockHeight, date }) => (
-            <div key={date} className="halving-date">{comma(blockHeight)}</div>
-          ))}
-        </li>
-        <li className="halving-data-card__area__list__item">
-          <div className="halving-reward">채굴 보상(단위: btc)</div>
-          {blockHalvingData.map(({ blockReward, date }, idx) => (
-            <div key={date} className="halving-date">{blockReward}</div>
-          ))}
-        </li>
+            <div key={label} className="halving-data-card__area__list__item">
+              <div className="halving__label">{label}</div>
+              {items.map(({value, key})=> (
+                <div key={key} className="halving__text">{value}</div>
+              ))}
+            </div>
+
+          ))
+        }
       </ul>
     </KAccordion>
   );
