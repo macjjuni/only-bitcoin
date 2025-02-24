58a2629300113a7e6f5a9983837cf1ca3601d604
macjjuni <macjjuni@gmail.com>
2025-02-24 22:47:27 +0900

    feature. 에러 페이지 작성

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,7 +2,7 @@ import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Client from "@/app/Client";
 import withSuspense from "@/widgets/hoc/withSuspence";
-
+import ErrorPage from "@/pages/errorPage/ErrorPage";
 
 const OverviewPage = withSuspense(lazy(() => import("@/pages/overviewPage/OverviewPage")));
 const BlocksPage = withSuspense(lazy(() => import("@/pages/blocksPage/BlocksPage")));
@@ -10,9 +10,8 @@ const Btc2FiatPage = withSuspense(lazy(() => import("@/pages/btc2fiatPage/Btc2Fi
 const PremiumPage = withSuspense(lazy(() => import("@/pages/premiumPage/PremiumPage")));
 const SettingsPage = withSuspense(lazy(() => import("@/pages/settingsPage/SettingsPage")));
 
-
 const clientRoutes = [
-  { title: "Overview", path: "/", element: <OverviewPage />, isNav: false, isFavorite: false, icon: { name: 'dashboard', size: 30 } },
+  { title: "Overview", path: "/", element: <OverviewPage />, isNav: false, isFavorite: false, icon: { name: 'dashboard', size: 30 }  },
   { title: "Overview", path: "/overview", element: <OverviewPage />, isNav: true, isFavorite: true, icon: { name: 'dashboard', size: 30 } },
   { title: "Blocks", path: "/blocks", element: <BlocksPage />, isNav: true, isFavorite: true, icon: { name: 'block', size: 32 }},
   { title: "BTC To Fiat", path: "/btc2fiat", element: <Btc2FiatPage />, isNav: true, isFavorite: true,icon: { name: 'exchange', size: 40 }, style: { padding: '3px' } },
@@ -26,7 +25,9 @@ const browserRouter = createBrowserRouter([
   {
     path: "/",
     Component: Client,
-    children: clientRoutes.map(({ title, isNav, icon, style, ...rest }) => rest)
+    children: clientRoutes.map(({ title, isNav, icon, style, ...rest }) => ({
+      ...rest, errorElement: <ErrorPage/>
+    }))
   }
 ]);
 
