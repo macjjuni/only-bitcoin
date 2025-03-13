4b99092c78fbb6ad13a1e078ac74c2f33012b046
macjjuni <macjjuni@gmail.com>
2025-03-13 17:26:57 +0900

    feature. 페이지 이동 애니메이션 기능 구현 #2
    - 페이지에서 쉽게 사용할 수 있도록 usePageAnimation 커스텀 훅 작성

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -2,3 +2,4 @@
 export{ default as useTitle} from "@/shared/hooks/useTitle";
 export{ default as useInitializer} from "@/shared/hooks/useInitializer";
 export{ default as useCopyOnClick} from "@/shared/hooks/useCopyOnClick";
+export{ default as usePageAnimation} from "@/shared/hooks/usePageAnimation";
