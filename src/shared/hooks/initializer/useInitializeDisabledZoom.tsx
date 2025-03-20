98db652b93c30f5e5d7a7090db30f26ec8d1e4ea
macjjuni <macjjuni@gmail.com>
2025-03-20 15:18:05 +0900

    feature. useInitializeDisabledZoom 로직 수정
    - 미설정 예외 페이지 적용

--- src/shared/hooks/initializer/useInitializeDisabledZoom.tsx
+++ src/shared/hooks/initializer/useInitializeDisabledZoom.tsx
@@ -1,10 +1,13 @@
 import { useCallback, useEffect } from "react";
-
+import { useLocation } from "react-router";
 
 export default function useInitializeDisabledZoom() {
 
 
-  // region [Privates]
+  // region [Hooks]
+
+  const locate = useLocation();
+
   // endregion
 
 
@@ -14,14 +17,16 @@ export default function useInitializeDisabledZoom() {
     e.preventDefault();
   }, []);
 
-  const disabledZoomEvent = useCallback(() => {
-    document.addEventListener("gesturestart" as unknown as keyof WindowEventMap, preventEvent, { passive: false });
-    document.addEventListener("dblclick", preventEvent, { passive: false });
-  }, []);
 
   const enabledZoomEvent = useCallback(() => {
     document.removeEventListener("gesturestart" as unknown as keyof WindowEventMap, preventEvent);
     document.removeEventListener("dblclick", preventEvent);
+  }, []);
+
+  const disabledZoomEvent = useCallback(() => {
+    enabledZoomEvent();
+    document.addEventListener("gesturestart" as unknown as keyof WindowEventMap, preventEvent, { passive: false });
+    document.addEventListener("dblclick", preventEvent, { passive: false });
   }, []);
 
   // endregion
@@ -30,12 +35,20 @@ export default function useInitializeDisabledZoom() {
   // region [LifeCycles]
 
   useEffect(() => {
+
     disabledZoomEvent();
 
-    return () => {
-      enabledZoomEvent();
-    }
+    return () => { enabledZoomEvent(); };
   }, []);
+
+  useEffect(() => {
+
+    if (locate.pathname.includes('meme')) {
+      enabledZoomEvent();
+    } else {
+      disabledZoomEvent();
+    }
+  }, [locate.pathname]);
 
   // endregion
 }
