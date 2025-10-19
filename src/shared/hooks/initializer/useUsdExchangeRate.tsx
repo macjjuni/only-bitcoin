a66384e0f428e729c1ef7e219aae766c5b812cd7
macjjuni <macjjuni@gmail.com>
2025-10-19 21:20:21 +0900

    feat. USDT 기준 환율 적용 해제 후 데이터 초기화 오류 수정

--- src/shared/hooks/initializer/useUsdExchangeRate.tsx
+++ src/shared/hooks/initializer/useUsdExchangeRate.tsx
@@ -71,7 +71,7 @@ export default function useUsdExchangeRate() {
       }
     }
     onError();
-  }, [query.data]);
+  }, [query.data, isUsdtStandard]);
 
   useEffect(() => {
     if (query.error) {
