a8ace0ddd3c1b81c850837dbcf09c42c249b1259
macjjuni <macjjuni@gmail.com>
2025-10-19 15:46:58 +0900

    trivial. 자잘한 스타일 수정

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,13 +1,16 @@
 export { default as AnimationIcon } from '@/components/icon/AnimationIcon'
-export { default as MemeIcon } from '@/components/icon/MemeIcon'
 export { default as MessageIcon } from '@/components/icon/MessageIcon'
 export { default as IosShareIcon } from '@/components/icon/IosShareIcon'
+export { default as DiscordIcon } from '@/components/icon/DiscordIcon'
+export { default as NaverIcon } from '@/components/icon/NaverIcon'
+export { default as PageIcon } from '@/components/icon/PageIcon'
+export { default as LinkIcon } from '@/components/icon/LinkIcon'
 
+
+// Navigation Icon
 export { default as DashboardIcon } from '@/components/icon/DashboardIcon'
 export { default as BlockIcon } from '@/components/icon/BlockIcon'
 export { default as ExchangeIcon } from '@/components/icon/ExchangeIcon'
 export { default as PremiumIcon } from '@/components/icon/PremiumIcon'
 export { default as SettingIcon } from '@/components/icon/SettingIcon'
 export { default as PillIcon } from '@/components/icon/PillIcon'
-export { default as DiscordIcon } from '@/components/icon/DiscordIcon'
-export { default as NaverIcon } from '@/components/icon/NaverIcon'
