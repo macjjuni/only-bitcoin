beff15ff1e077b535b795b7e831669a38e17064f
macjjuni <macjjuni@gmail.com>
2025-12-29 14:02:32 +0900

    feat. Tailwind 마이그레이션 #15

--- src/pages/btc2fiatPage/components/premiumField/PremiumField.tsx
+++ src/pages/btc2fiatPage/components/premiumField/PremiumField.tsx
@@ -31,7 +31,7 @@ const PremiumField = () => {
         <b className="text-base">Premium</b>
         <span className="text-sm opacity-80">P(%):</span>
       </div>
-      <KNumberStepper value={premium} onChange={setPremium} min={0} step={0.1} precision={1} max={100} size="lg" />
+      <KNumberStepper value={premium} onChange={setPremium} min={0} step={0.1} max={100} size="lg" />
     </div>
   );
 };
