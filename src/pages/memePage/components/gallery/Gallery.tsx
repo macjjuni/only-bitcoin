5f2c1fa863d2a7b7c68da5d061f6a16bfebf40e3
macjjuni <macjjuni@gmail.com>
2026-01-05 11:34:06 +0900

    feat. 밈 이미지 스타일 수정

--- src/pages/memePage/components/gallery/Gallery.tsx
+++ src/pages/memePage/components/gallery/Gallery.tsx
@@ -24,7 +24,7 @@ const Gallery = ({ images, selected }: GalleryProps) => {
 
 
   return (
-    <div className="flex gap-4 px-2 pb-3 justify-center z-[-1]">
+    <div className="flex gap-4 px-2 pb-3 justify-center">
       <div className="flex-1 flex flex-col gap-4 max-w-[237px] w-[calc((100dvw-48px)/2)]">
         {filterImages
           .filter((_, index) => index % 2 === 0)
