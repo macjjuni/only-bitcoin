85b18a350d40bca7562df9d48817f5535302d503
macjjuni <macjjuni@gmail.com>
2026-01-10 21:31:02 +0900

    feat. overview 페이지 ssr 적용

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import { MacroWidgetPanel, MarketChart, MiningMetricChart } from "./components";
+import { BlockHalvingCard, MacroWidgetPanel, MarketChart, MiningMetricChart, PricePanel } from "./components";
 import useStore from "@/shared/stores/store";
 import { PageLayout } from "@/layouts";
-import { KSkeleton } from "kku-ui";
 
 
-const BlockHalvingCard = dynamic(() =>
-    import("@/app/overview/components/blockHalvingCard/BlockHalvingCard"),
-  { ssr: false, loading: () => <KSkeleton className="w-full h-[132px]" /> }
-);
-
-const PricePanel = dynamic(() =>
-    import("@/app/overview/components/pricePanel/PricePanel"),
-  { ssr: false, loading: () => <KSkeleton className="w-full h-[74px]" /> }
-);
+// const BlockHalvingCard = dynamic(() =>
+//     import("@/app/overview/components/blockHalvingCard/BlockHalvingCard"),
+//   { ssr: false, loading: () => <KSkeleton className="w-full h-[132px]" /> }
+// );
+//
+// const PricePanel = dynamic(() =>
+//     import("@/app/overview/components/pricePanel/PricePanel"),
+//   { ssr: false, loading: () => <KSkeleton className="w-full h-[74px]" /> }
+// );
 
 export default function OverviewPage() {
 
