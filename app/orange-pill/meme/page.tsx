556d95867c140b5cb2ab00c07532a10c9df9d1d7
macjjuni <macjjuni@gmail.com>
2026-01-19 20:17:15 +0900

    feat. 밈 페이지 메타태그 수정

--- app/orange-pill/meme/page.tsx
+++ app/orange-pill/meme/page.tsx
@@ -5,7 +5,24 @@ import { env } from "@/shared/config/env";
 
 export const metadata: Metadata = {
   title: `${env.NEXT_PUBLIC_TITLE} - Meme`,
-  description: "비트코인 밈과 짤방을 확인하고 공유하세요."
+  description: "비트맥시를 위한 성지",
+  openGraph: {
+    type: "website",
+    url: env.NEXT_PUBLIC_URL,
+    title: "비트맥시 전용 밈 저장소",
+    description: "비트맥시를 위한 성지",
+    images: [
+      {
+        url: "/app/og-image-meme.webp"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "비트맥시 전용 밈 저장소",
+    description: "비트맥시를 위한 성지",
+    images: ["/app/og-image-meme.webp"]
+  },
 };
 
 export default async function Page() {
