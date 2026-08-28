f06858391a2bac74a4708734ceff71e40e742aef
macjjuni <macjjuni@gmail.com>
2026-08-28 23:52:47 +0900

    서비스 페이지 아이콘 색상 개선

--- src/shared/ui/icon/BuildingIcon.tsx
+++ src/shared/ui/icon/BuildingIcon.tsx
@@ -18,24 +18,36 @@ const BuildingIcon = ({ size = "100%", color = "currentColor", style, className 
         transform="translate(-5.5 -5.5) scale(2.0625)"
       />
 
-      <g
-        fill="none"
-        stroke="hsl(var(--foreground))"
-        strokeWidth="1.8"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        transform="translate(12.2 12.2) scale(2.0625)"
-      >
+      <g transform="translate(16 16) scale(0.0390625)">
+        <path d="M224 96h576v928H224z" fill="#EAEAEA" />
         <path
-          d="M5 21V6a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v3h5a1 1 0 0 1 1 1v11Z"
-          fill="hsl(var(--background))"
+          d="M288 96h64v672H288zM416 96h64v672h-64zM544 96h64v672h-64zM672 96h64v672h-64z"
+          fill="#469FCC"
         />
-        <path d="M3 21h18" />
-        <path d="M8 9h2" />
-        <path d="M8 13h2" />
-        <path d="M8 17h2" />
-        <path d="M16 13h0" />
-        <path d="M16 17h0" />
+        <path
+          d="M288 832h64v192H288zM416 832h64v192h-64zM544 832h64v192h-64zM672 832h64v192h-64z"
+          fill="#434854"
+        />
+        <path
+          d="M864 65.984c0 16.576-13.44 30.016-30.016 30.016H190.016A30.016 30.016 0 0 1 160 65.984V30.016C160 13.44 173.44 0 190.016 0h644c16.544 0 29.984 13.44 29.984 30.016v35.968z"
+          fill="#EF4D4D"
+        />
+        <path
+          d="M224 96h64v32H224zM352 96h64v32h-64zM480 96h64v32h-64zM608 96h64v32h-64zM736 96h64v32h-64z"
+          fill="#000000"
+        />
+        <path
+          d="M288 96h64v32H288zM416 96h64v32h-64zM544 96h64v32h-64zM672 96h64v32h-64z"
+          fill="#000000"
+        />
+        <path
+          d="M238.24 1024A126.656 126.656 0 0 0 256 960a128 128 0 0 0-256 0c0 23.424 6.752 45.088 17.76 64h220.48zM896 832a127.744 127.744 0 0 0-116.224 75.04A94.848 94.848 0 0 0 736 896a96 96 0 0 0-96 96c0 11.296 2.304 21.952 5.888 32h360.384A126.944 126.944 0 0 0 1024 960a128 128 0 0 0-128-128z"
+          fill="#3AAD73"
+        />
+        <path
+          d="M779.776 907.04A94.848 94.848 0 0 0 736 896a96 96 0 0 0-96 96c0 11.296 2.304 21.952 5.888 32h139.872A126.656 126.656 0 0 1 768 960c0-18.944 4.384-36.768 11.776-52.96z"
+          fill="#000000"
+        />
       </g>
     </svg>
   );
